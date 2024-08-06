f0051a117ffed32edf4fe20a346f8a712c7001a6
cx3576888 <cx3576888@gmail.com>
2024-08-07 01:20:09 +0800

    add GameBoard component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import styles from '../styles/App.module.css';
 import Timer from './Timer';
+import GameBoard from './GameBoard';
 import GameRule from './GameRule';
 
 const App: React.FC = () => {
   return (
     <div data-testid="app-testid" className={styles.app}>
       <Timer />
+      <GameBoard n={6} />
       <GameRule />
     </div>
   );
---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,46 @@
+import styles from '../styles/GameBoard.module.css';
+
+interface GameBoardProps {
+  n: number;
+}
+
+interface GameCell {
+  id: string;
+  row: number;
+  col: number;
+}
+
+const GameBoard: React.FC<GameBoardProps> = ({ n }) => {
+  const rows: GameCell[][] = [];
+  for (let i = 0; i < n; i++) {
+    rows[i] = [];
+    for (let j = 0; j < n; j++) {
+      const cell = {
+        id: `${i + 1}-${j + 1}`,
+        row: i + 1,
+        col: j + 1
+      }
+      rows[i]!.push(cell);
+    }
+  }
+  
+  return (
+    <div className={styles.gameBoard}>
+      {rows.map((row, i) => {
+        return (
+          <div key={'row'+(i+1)} className={styles.gameBoardRow}>
+            {row.map(cell => {
+              return (
+                <div key={cell.id} className={styles.gameCell}>
+                  row: {cell.row}, col: {cell.col}
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default GameBoard;

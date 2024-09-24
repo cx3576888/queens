ffe2e95c86563d74fbe43e47bb177e7c4eddfa94
cx3576888 <cx3576888@gmail.com>
2024-09-24 15:29:26 +0800

    add puzzles, fix duplicate puzzle

--- src/utils/puzzleUtils.ts
+++ src/utils/puzzleUtils.ts
@@ -1,6 +1,6 @@
 import Cell from '../models/cellModel';
 
-export const latestPuzzleNumber = 143;
+export const latestPuzzleNumber = 147;
 export const numberOfTestPuzzles = 2;
 
 export const getPuzzleNumbers = () => {

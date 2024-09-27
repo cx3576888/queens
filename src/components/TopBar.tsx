696f2c7a1ec759801523ca74d6cd4fbfc0e8432d
cx3576888 <cx3576888@gmail.com>
2024-09-28 00:43:28 +0800

    ok part

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,6 +2,7 @@ import type { RootState } from '../state/store';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearBoard } from '../state/slices/boardSlice';
 import { setStatus } from '../state/slices/timerSlice';
+import SettingsDropdown from './SettingsDropdown';
 import Timer from './Timer';
 
 import styles from '../styles/TopBar.module.css';
@@ -20,7 +21,10 @@ const TopBar: React.FC = () => {
 
   return (
     <div className={styles.topBar}>
-      <button disabled={status !== 'running'} onClick={handlePause}>Pause</button>
+      <div className={styles.topBarLeft}>
+        <SettingsDropdown />
+        <button disabled={status !== 'running'} onClick={handlePause}>Pause</button>
+      </div>
       <Timer />
       <button disabled={status !== 'running'} onClick={handleClearBoard}>Clear Board</button>
     </div>

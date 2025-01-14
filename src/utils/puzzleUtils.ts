ba56484e3e5a80993eca011216d653c706f1d3b5
cx3576888 <cx3576888@gmail.com>
2025-01-14 23:32:18 +0800

    daily puzzle update

--- src/utils/puzzleUtils.ts
+++ src/utils/puzzleUtils.ts
@@ -1,6 +1,6 @@
 import Cell from '../models/cellModel';
 
-export const latestPuzzleNumber = 252;
+export const latestPuzzleNumber = 259;
 export const numberOfTestPuzzles = 2;
 
 export const getPuzzleNumbers = () => {
